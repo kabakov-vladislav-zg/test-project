849028bca9c95fdafb0f7f3740ae45130d2d9a43
Vladislav Kabakov <zloy.gololed@gmail.com>
2023-08-10 19:35:32 +0400

    feat: VBtn

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ import VCard from '@/components/VCard.vue';
 import VInputAvatar from '@/components/VInputAvatar.vue';
 import VInputText from '@/components/VInputText.vue';
 import VRadio from '@/components/VRadio.vue';
+import VBtn from '@/components/VBtn.vue';
+import IconEdit from '@/components/icons/IconEdit.vue';
+import IconTrash from '@/components/icons/IconTrash.vue';
 
 const avatar = ref('');
 const nameFirst = ref('');
@@ -40,6 +43,24 @@ const radio = ref(false);
         v-model="radio"
         label="Показать фамилии"
       />
+      <VBtn>
+        Кнопка
+      </VBtn>
+      <VBtn
+        disabled
+      >
+        Кнопка
+      </VBtn>
+      <VBtn
+        ghost
+      >
+        <IconEdit />
+      </VBtn>
+      <VBtn
+        ghost
+      >
+        <IconTrash />
+      </VBtn>
     </VCard>
   </div>
 </template>
---
+++ src/components/VBtn.vue
@@ -0,0 +1,47 @@
+<script
+  setup
+  lang="ts"
+>
+const props = withDefaults(defineProps<{
+  type?: 'button' | 'submit' | 'reset'
+  disabled?: boolean
+  ghost?: boolean
+}>(), {
+  type: 'button',
+  disabled: false,
+});
+</script>
+
+<template>
+  <button
+    :type="type"
+    :disabled="disabled"
+    class="VBtn rounded-lg"
+    :class="{ 'VBtn_ghost' : ghost }"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style>
+.VBtn {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  text-decoration: none;
+  vertical-align: middle;
+  user-select: none;
+  cursor: pointer;
+  @apply bg-blue-600 text-white px-4 py-2;
+}
+.VBtn_ghost {
+  color: inherit;
+  @apply bg-transparent text-inherit p-2;
+}
+.VBtn:disabled {
+  cursor: auto;
+  pointer-events: none;
+  filter: grayscale(1) brightness(220%);
+}
+</style>
