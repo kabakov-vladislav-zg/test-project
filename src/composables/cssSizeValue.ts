9f6c1de1d0e6e265224b954ba2a772e313765c4b
Vladislav Kabakov <zloy.gololed@gmail.com>
2023-08-12 18:00:49 +0400

    refactor(useCssSizeValue): improve tiping

--- src/composables/cssSizeValue.ts
+++ src/composables/cssSizeValue.ts
@@ -1,13 +1,13 @@
 import type { MaybeRefOrGetter } from 'vue';
 import { computed, toValue } from 'vue';
 
-export function useCssSizeValue(size: MaybeRefOrGetter) {
+export function useCssSizeValue(size: MaybeRefOrGetter<string | number>) {
   const value = computed(() => {
     let value = toValue(size);
     if (Number(value)) {
       return `${value}px`;
     } else {
-      return value;
+      return String(value); // TODO perhaps should adding validation
     }
   });
   return { value };
