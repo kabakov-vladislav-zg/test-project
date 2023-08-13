9214f850a050476ea469388868616b48689db6e2
Vladislav Kabakov <zloy.gololed@gmail.com>
2023-08-13 17:56:31 +0400

    refactor: remove keys/user

--- src/keys/user.ts
+++
@@ -1,7 +0,0 @@
-import type { InjectionKey } from 'vue'
-
-export const userKey = Symbol() as InjectionKey<string>;
-export interface UserProvide {
-  changeUser: (id?: number) => void
-  deleteUser: (id: number) => void
-}
