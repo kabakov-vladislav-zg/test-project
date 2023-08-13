fed45eed3149197c85c9a158bed27e856e8dbec9
Vladislav Kabakov <zloy.gololed@gmail.com>
2023-08-13 12:40:38 +0400

    refactor(VInputCheckbox): simplify

--- src/components/VInputCheckbox.vue
+++ src/components/VInputCheckbox.vue
@@ -3,7 +3,6 @@
   lang="ts"
 >
 import { computed } from 'vue';
-import VCheckbox from '@/components/VCheckbox.vue';
 
 const props = defineProps<{
   modelValue?: boolean
@@ -24,46 +23,52 @@ const inputValue = computed({
 
 <template>
   <label class="VInputCheckbox">
-      <VCheckbox
-        v-model="inputValue"
-        class="VInputCheckbox__toggler"
-      />
-      <span class="VInputCheckbox__state"></span>
-      <span class="VInputCheckbox__label">
-        {{ label }}
-      </span>
+    <input
+      v-model="inputValue"
+      type="checkbox"
+      class="VInputCheckbox__toggler"
+    />
+    <span class="VInputCheckbox__state"></span>
+    <span class="VInputCheckbox__label">
+      {{ label }}
+    </span>
   </label>
 </template>
 
 <style>
 .VInputCheckbox {
-    display: inline-flex;
-    align-items: center;
+  display: inline-flex;
+  align-items: center;
 }
 .VInputCheckbox__state {
-    width: 44px;
-    height: 24px;
-    border-radius: 12px;
-    display: flex;
-    padding: 2px;
-    margin-right: 16px;
-    margin-bottom: 2px;
-    cursor: pointer;
-    transition: background-color ease 0.25s;
-    @apply bg-gray-200;
+  width: 44px;
+  height: 24px;
+  border-radius: 12px;
+  display: flex;
+  padding: 2px;
+  margin-right: 16px;
+  margin-bottom: 2px;
+  cursor: pointer;
+  transition: background-color ease 0.25s;
+  @apply bg-gray-200;
 }
 .VInputCheckbox__state::before {
-    content: '';
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    transition: transform ease 0.25s;
-    @apply bg-white;
+  content: '';
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  transition: transform ease 0.25s;
+  @apply bg-white;
+}
+.VInputCheckbox__toggler {
+  position: absolute;
+  clip: rect(0,0,0,0);
+  pointer-events: none;
 }
 .VInputCheckbox__toggler:checked + .VInputCheckbox__state {
-    @apply bg-blue-600;
+  @apply bg-blue-600;
 }
 .VInputCheckbox__toggler:checked + .VInputCheckbox__state::before {
-    transform: translateX(20px);
+  transform: translateX(20px);
 }
 </style>
