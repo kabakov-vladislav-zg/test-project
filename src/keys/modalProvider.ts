a8cbf89e99ba43e331a893a10857153cdc8b12a1
Vladislav Kabakov <zloy.gololed@gmail.com>
2023-08-13 18:35:07 +0400

    feat(modalProvider): typing component props

--- src/keys/modalProvider.ts
+++ src/keys/modalProvider.ts
@@ -1,12 +1,13 @@
 import type { InjectionKey, Component } from 'vue'
+import type { ComponentProps, ComponentEmit } from 'vue-component-type-helpers'
 
-export interface ModalOptions {
-  component: Component
-  bindings?: object | null
-  events?: object | null
+export interface ModalOptions<T extends Component> {
+  component: T
+  bindings?: ComponentProps<T>  | null // TODO Need to merge with attributes interface? What is the interface of attributes?
+  events?: object | null // TODO Typing events.
 }
 export interface ModalProvider {
-  open: (options: ModalOptions) => void
+  open: <T extends Component>(options: ModalOptions<T>) => void
   close: () => void
 }
 export const modalKey = Symbol() as InjectionKey<ModalProvider>;
